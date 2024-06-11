f33b6430a1a5e7ba08811fa18a3b6bc0cecbbe0e
perun <lbrzozowski@gmail.com>
2024-06-11 16:20:44 +0200

    ssl

--- proxy.js
+++ proxy.js
@@ -1,15 +1,28 @@
+const https = require("https");
+const fs = require("fs");
 const express = require('express')
 const cors = require('cors')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
+const port = 3000
 const app = express()
-
+https.createServer(
+	{
+		key: fs.readFileSync("ssl/test.key"),
+		cert: fs.readFileSync("ssl/test.crt"),
+	},
+	app
+)
+	.listen(port, '0.0.0.0', ()=>{
+		console.log(`CORS proxy server is running at https, port ${port}`);
+	});
 app.use(cors())
 
 app.use(
   '/proxy',
   createProxyMiddleware({
-    target: 'http://127.0.0.1:9000',
+//	target: 'http://127.0.0.1:9005',
+    target: 'http://asr-webservice:9000',
     changeOrigin: true,
     pathRewrite: {
       '^/proxy': '',
@@ -17,7 +30,6 @@ app.use(
   })
 )
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`CORS proxy server is running at http://localhost:${port}`)
-})
+//app.listen(port, '0.0.0.0', () => {
+//  console.log(`CORS proxy server is running at http://localhost:${port}`)
+//})
